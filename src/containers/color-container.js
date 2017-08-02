57266023eb5aede7db19de0d13b07df54b7453c1
Seth Watson <seth.watson1@gmail.com>
2017-08-02 14:18:32 -0600

    Added css folder, modified color-container

---
+++ src/containers/color-container.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ColorContainer extends React.Component {
+    render() {
+        return (
+            <div className="box" style={{ backgroundColor: "blue" }}>
+                <button>Color</button>
+            </div>
+        )
+    }
+}
+
+export default ColorContainer;
+
